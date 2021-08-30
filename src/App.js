478094c68138eb9e0d4c6bb44da3fdc44b58104b
Ronniel de Ramos <ronnielderamos@gmail.com>
2021-08-30 15:00:23 +0800

    login, register function

---
+++ src/App.js
@@ -0,0 +1,38 @@
+//TODO:
+// Registration
+// Login
+// Send Message
+// Retrieve Message
+// Create Channel
+// Get all users
+// Get channel details via channel ID
+// Add member to a channel
+// List of All Users
+
+import React, { useState } from 'react';
+import LogInForm from './components/login/LogInForm';
+import RegistrationForm from './components/registration/RegistrationForm';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import MainPage from './components/main/MainPage';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn]= useState(false);
+
+  function toggleIsLoggedIn(){
+    setIsLoggedIn(!isLoggedIn)
+  }
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {isLoggedIn?<MainPage toggleIsLoggedIn={toggleIsLoggedIn} />:<LogInForm toggleIsLoggedIn={toggleIsLoggedIn} />}
+        </Route>
+        <Route path="/register">
+          <RegistrationForm />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
